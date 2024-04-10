55d27d475f072a16b6c85b57b4983aa0ab27dd02
Isac Pessoa <isacdavidpessoa@hotmail.com>
2024-04-10 18:42:05 -0300

    Estilizando barra lateral de busca

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,3 +1,11 @@
-const BarraLateral = () => <aside>Barra lateral</aside>
+import * as S from './styles'
+
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <S.Campo type="text" placeholder="Pesquisar" />
+    </div>
+  </S.Aside>
+)
 
 export default BarraLateral
---
+++ src/containers/BarraLateral/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Aside = styled.aside`
+  padding: 16px;
+  background-color: #d9d9ed;
+  height: 100vh;
+`
+
+export const Campo = styled.input`
+  padding: 8px;
+  border-radius: 8px;
+  font-weight: bold;
+  background-color: #fff;
+  color: #111;
+  border-color: #b2b3da;
+  width: 100%;
+`
