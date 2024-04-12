6792c7a7a112ed372a1d5b47a65a96c9aa6ab996
Isac Pessoa <isacdavidpessoa@hotmail.com>
2024-04-12 17:17:56 -0300

    Configurando filtro

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,11 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux'
 import * as S from './styles'
+import { RootReducer } from '../../store'
+import { alteraTermo } from '../../store/reducers/filtro'
 
-const BarraLateral = () => (
-  <S.Aside>
-    <div>
-      <S.Campo type="text" placeholder="Pesquisar" />
-    </div>
-  </S.Aside>
-)
+const BarraLateral = () => {
+  const dispatch = useDispatch()
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  return (
+    <S.Aside>
+      <div>
+        <S.Campo
+          type="text"
+          placeholder="Pesquisar"
+          value={termo}
+          onChange={(evento) => dispatch(alteraTermo(evento.target.value))}
+        />
+      </div>
+    </S.Aside>
+  )
+}
 
 export default BarraLateral
