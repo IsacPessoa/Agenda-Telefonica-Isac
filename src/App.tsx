ccbe2bdedc2ed315c902020a1e7d48ce1754d79d
Isac Pessoa <isacdavidpessoa@hotmail.com>
2024-04-12 17:46:15 -0300

    criando botao e pagina de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Estiloglobal, { Container } from './styles/index '
 
 import store from './store'
 import Home from './pages/Home'
+import Cadastro from './pages/Cadastro'
 
 const rotas = createBrowserRouter([
   {
@@ -13,7 +14,7 @@ const rotas = createBrowserRouter([
   },
   {
     path: '/novo',
-    element: <h1>Novo Contato</h1>
+    element: <Cadastro />
   }
 ])
 
